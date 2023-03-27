90ece672908f7dfe3cc343470836cfedd7e2d9ee
Antichess <cahalng@gmail.com>
2023-03-27 01:31:00 -0400

    Update count_thread_reply_times.py
    shit edit lol

--- count_thread_reply_times.py
+++ count_thread_reply_times.py
@@ -1,53 +1,51 @@
 import csv
 
 rawdata = []
-with open("LOG_4980001to4981000.csv","r") as file:
+with open("LOG_5035001to5036000.csv","r") as file:
     reader = csv.reader(file)
 
     for row in reader:
         rawdata.append(row)
-   
-def get_times(x):
-    seconds = x
-    leftover = seconds % 86400
-    seconds -= leftover
-    days = seconds / 86400
-    seconds = leftover
-    leftover = seconds % 3600
-    seconds -= leftover
-    hours = seconds / 3600
-    seconds = leftover
-    leftover = seconds % 60
-    seconds -= leftover
-    mins = seconds / 60
-    seconds = leftover
-    timestr = ""
-    if days > 0:
-        timestr = timestr + str(int(days)) + "d "
-    if hours > 0:
-        timestr = timestr + str(int(hours)) + "h "
-    if mins > 0:
-        timestr = timestr + str(int(mins)) + "m "
-    timestr = timestr + str(int(seconds)) + "s "
-    return timestr
-    
+
+alts = []
+with open("../input/prefs/Aliases.txt","r") as f:
+    alts = f.read()
+
+alts = alts.split("\n")
+alts = [x.split(",") for x in alts]
 
 
+def alt(user):
+    for x in alts:
+        if x[0] == user:
+            return user
+        if user in x[1::]:
+            return x[0]
+    return user
+
+def get_time(d):
+    if d < 60:
+        return f"{d%60}s "
+    if d < 3600:
+        return f"{d//60}m {d%60}s "
+    else:
+        return f"{d//3600}h {d%3600//60}m {d%3600%60}s "
+            
 userstats = []
 for i in range(len(rawdata)-1):
     found = False
     append = []
     for x in userstats:
-        if x[0] == rawdata[i+1][1]:
+        if x[0] == alt(rawdata[i+1][1]):
             if x[1] == (int(float(rawdata[i+1][2])) - int(float(rawdata[i][2]))):                
                 found = True
                 break
     if found is False:
-        append = (rawdata[i+1][1],(int(float(rawdata[i+1][2])) - int(float(rawdata[i][2]))),1)
+        append = (alt(rawdata[i+1][1]),(int(float(rawdata[i+1][2])) - int(float(rawdata[i][2]))),1)
         userstats.append(append)
     else: #if found is true
         for j in range(len(userstats)):
-            if (userstats[j][0] == rawdata[i+1][1]):
+            if (userstats[j][0] == alt(rawdata[i+1][1])):
                 if (userstats[j][1] == (int(float(rawdata[i+1][2])) - int(float(rawdata[i][2])))):
                      temp = list(userstats[j])
                      temp[2] = temp[2] + 1
@@ -79,7 +77,7 @@ for y in unique_names:
     for t in times:
         for x in userstats:
             if y == x[0] and t == x[1] and x[2] != 0:
-                print(str(x[0]) + " | " + get_times(t) + "| " + str(x[2]))
+                print(str(alt(x[0])) + " | " + get_time(t) + "| " + str(x[2]))
                 break
 
 
@@ -87,7 +85,7 @@ print()
 print("Username|Reply time sum")
 print("---|---")
 
-user_sums.sort(key = lambda x: x[1])
-for x in user_sums:
+user_sums.sort(key = lambda x: x[1],reverse=True)
+for x in user_sums[0:2]:
     
-    print(x[0] + " | " + get_times(x[1]))
+    print(alt(x[0]) + " | " + get_time(x[1]))
