aece291aae7a860d94ed72f06858bddbee6389ce
Antichess <cahalng@gmail.com>
2023-04-13 01:14:07 -0400

    Update 0s.py

--- 0s.py
+++ 0s.py
@@ -2,33 +2,34 @@ import csv
 
 
 rawdata = []
-with open("ALL.csv","r") as file:
+with open("decimal_log.csv","r") as file:
     reader = csv.reader(file)
 
     for row in reader:
         rawdata.append(row)
 
 
-users = []
+users = {}
+def add_data(u):
+    if u not in users:
+        users[u] = 1
+    else:
+        users[u] = users[u] + 1
 diff = 0
+# difference to test for
 
 for x in range(len(rawdata)-1):
     try:
-        if (int(float(rawdata[x][2]))+diff == int(float(rawdata[x+1][2]))):
-            found = False
-            for i in range(len(users)):
-                if rawdata[x+1][1] == users[i][0]:
-                    users[i] = (users[i][0], users[i][1] + 1)
-                    found = True
-                    break
-            if not found:
-                append = (rawdata[x+1][1], 1)
-                users.append(append)
+        if int(float(rawdata[x][2])) == int(float(rawdata[x-1][2])) + diff:
+            add_data(rawdata[x][1])
     except:
         pass
 
-users.sort(key=lambda x:x[1],reverse=True)
+print(users)
+data = [[x,users[x]] for x in users]
+data.sort(key=lambda x:x[1],reverse=True)
+
 print("Rank|User|"+str(diff)+"s")
 print(":-:|:-:|:-:")
-for x in range(len(users)):
-    print(str(x+1) + "|"  + users[x][0] + "|" + str(users[x][1]))
+for c,x in enumerate(data):
+    print(f"{c+1}|{x[0]}|{x[1]}")
